2af24d7b540978275a910bae35c38412ea934906
Bijan <bijanbwb@gmail.com>
2018-01-28 00:24:24 -0500

    Initial working Presence feature

--- lib/platform_web/channels/score_channel.ex
+++ lib/platform_web/channels/score_channel.ex
@@ -13,11 +13,15 @@ defmodule PlatformWeb.ScoreChannel do
   use PlatformWeb, :channel
   use Appsignal.Instrumentation.Decorators
 
+  alias Platform.GamePresence
   alias Platform.Products
 
   def join("score:" <> game_slug, _payload, socket) do
     game = Products.get_game_by_slug!(game_slug)
     socket = assign(socket, :game_id, game.id)
+
+    send(self(), :after_join)
+
     {:ok, socket}
   end
 
@@ -31,6 +35,16 @@ defmodule PlatformWeb.ScoreChannel do
 
     Products.create_gameplay(payload)
     broadcast(socket, "save_score", payload)
+    {:noreply, socket}
+  end
+
+  def handle_info(:after_join, socket) do
+    push(socket, "presence_state", GamePresence.list(socket))
+
+    {:ok, _} = GamePresence.track(socket, socket.assigns.player_id, %{
+      online_at: inspect(System.system_time(:seconds))
+    })
+
     {:noreply, socket}
   end
 end
